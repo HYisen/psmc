c30539741b34b44830852c51525117575cd9de39
alex <alexhyisen@gmail.com>
2023-08-09 21:20:19 +0800

    add getPIDs

--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("Hello world.")
+	pids, err := getPIDs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(pids)
+}
+
+const procPathRoot = "/proc"
+
+func getPIDs() ([]int, error) {
+	dirEntries, err := os.ReadDir(procPathRoot)
+	if err != nil {
+		return nil, err
+	}
+
+	var ret []int
+	for _, entry := range dirEntries {
+		num, err := strconv.Atoi(entry.Name())
+		if err != nil {
+			continue
+		}
+		ret = append(ret, num)
+	}
+	return ret, nil
 }
